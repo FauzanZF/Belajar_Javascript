351b2a36a0a21a11238c9196a74e2965d4a3dbe2
unknown <fauzanzacky1212@gmail.com>
2024-11-08 12:39:32 +0700

    menambah materi

---
+++ 1SyntaxDasar.js
@@ -0,0 +1,27 @@
+let nama = ' Fauzan'
+let usia = 19
+let tinggiBadan = 170
+let beratBadan  //udefined
+let pacar = null
+
+beratBadan = 60
+
+if (pacar == null){
+    pacar = 'tidak punya'
+}else{
+    pacar = 'ada'
+}
+
+let saldoAwal = 50000
+let saldoTambahan = 80000
+const saldoAkhir = saldoAwal + saldoTambahan
+
+alert(
+    `nama saya ${nama} usia saya ${usia} tahun tinggi badan saya ${tinggiBadan} cm berat badan saya ${beratBadan} kg pacar saya ${pacar},`
+);
+
+alert(
+    `saldo awal saya ${saldoAwal} saldo tambahan saya ${saldoTambahan} saldo akhir saya ${saldoAkhir}`,
+)
+
+alert('halo')
