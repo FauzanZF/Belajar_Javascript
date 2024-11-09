761eb3d9446adc04960e235c9cfa65f12a955117
unknown <fauzanzacky1212@gmail.com>
2024-11-09 14:37:34 +0700

    menambah materi

---
+++ 2Array.js
@@ -0,0 +1,12 @@
+let namaGuru = ['Pak Agus', 'Bu Dina', 'Pak Budi', 'Bu Eka']
+console.log(namaGuru[0])
+namaGuru.push('dea','asep') //menambah array
+namaGuru.shift() //hapus array pertama
+namaGuru.pop()  //hapus array terakhir
+alert(namaGuru)
+
+let namaMurid = []
+namaMurid[0] = 'Fauzan'
+namaMurid[1] = 'Jokowi'
+alert (namaMurid)
+
