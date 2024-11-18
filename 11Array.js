513832ed29179cbf16e1f4a1a727eb3f02644c48
unknown <fauzanzacky1212@gmail.com>
2024-11-18 12:14:57 +0700

    menambah materi

---
+++ 11Array.js
@@ -0,0 +1,20 @@
+const arraySaya = ['halohalo','😂','👌']
+arraySaya.push('🍏')
+console.log(arraySaya)
+
+//-------------------------------------------------------------------
+
+const arrayKamu = []
+arrayKamu[0] = 'tes' //menambah array
+arrayKamu[1] = '123456' 
+arrayKamu[1] = 'AWOKWOWOKWOKK' //mengganti index 1
+console.log(arrayKamu)
+
+//-------------------------------------------------------------------
+
+const arrayKita = new Array()
+arrayKita[0] = '💕'
+//array index 2 empty
+arrayKita[3] = '😎'
+arrayKita[1] = '🔥'
+console.log(arrayKita)
