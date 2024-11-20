57d6654c526b0a08fb6c4314be77319855858007
unknown <fauzanzacky1212@gmail.com>
2024-11-20 07:26:49 +0700

    menambah materi

---
+++ 13AliciaKey.js
@@ -0,0 +1,11 @@
+const arraySaya = []
+
+
+arraySaya['apple'] = '🍏'
+arraySaya['banana'] = '🍌'
+arraySaya['cherry'] = '🍒'
+arraySaya['dragonfruit'] = '🐉'
+
+console.log(arraySaya)
+console.log(arraySaya[0])
+console.log(arraySaya['apple'])
